746b6bb5b4d26c4a26439f21acc1d87301d3fcdf
@alitaham <alirazmjoo20@gmail.com>
2025-09-08 23:34:08 +0330

    start coding

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [
+     TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',   // اگر داکر یا سرور داری اینو تغییر بده
+      port: 5432,
+      username: 'postgres', // یوزرنیم دیتابیس
+      password: 'admin123456789', // پسورد دیتابیس
+      database: 'blog-db', // اسم دیتابیس
+      autoLoadEntities: true, // همه‌ی Entity ها رو خودکار لود میکنه
+      synchronize: true, // فقط در محیط توسعه فعال باشه
+    }),
+     PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/posts/entities/post.entity.ts
@@ -0,0 +1,68 @@
+// src/posts/post.entity.ts
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+export enum PostStatus {
+  DRAFT = 'draft',
+  PUBLISHED = 'published',
+  REVIEW = 'review',
+}
+
+@Entity('posts')
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  // عنوان
+  @Column({ length: 200 })
+  title: string;
+
+  // عنوان سئو
+  @Column({ length: 200, nullable: true })
+  seoTitle: string;
+
+  // اسلاگ (برای آدرس URL)
+  @Column({ length: 200, unique: true })
+  slug: string;
+
+  // متا دسکریپشن
+  @Column({ type: 'varchar', length: 300, nullable: true })
+  metaDescription: string;
+
+  // خلاصه مقاله
+  @Column({ type: 'text', nullable: true })
+  excerpt: string;
+
+  // محتوای مقاله
+  @Column({ type: 'text' })
+  content: string;
+
+  // تگ‌ها (رشته‌ای از کلمات کلیدی جدا شده با ویرگول)
+  @Column('simple-array', { nullable: true })
+  tags: string[];
+
+  // دسته‌بندی (فعلاً رشته، بعداً میشه رابطه جداگانه درست کرد)
+  @Column({ nullable: true })
+  category: string;
+
+  // تصویر بندانگشتی
+  @Column({ nullable: true })
+  thumbnail: string;
+
+  // تصویر کاور
+  @Column({ nullable: true })
+  coverImage: string;
+
+  // وضعیت مقاله
+  @Column({
+    type: 'enum',
+    enum: PostStatus,
+    default: PostStatus.DRAFT,
+  })
+  status: PostStatus;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
