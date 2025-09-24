69a8919bc8f649ee520a0209a212b11e99d78fae
@alitaham <alirazmjoo20@gmail.com>
2025-09-24 15:35:00 +0330

    change data-source

--- data-source.ts
+++ data-source.ts
@@ -4,11 +4,24 @@ import { DataSource } from 'typeorm';
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: 5432,
+  port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: ['src/entities/*.entity.ts'],
-  migrations: ['src/migrations/*.ts'],
+
+  // ✅ به جای ایمپورت مستقیم، مسیر دادی
+  entities: [
+    process.env.NODE_ENV === 'production'
+      ? 'dist/src/entities/*.entity.js'
+      : 'src/entities/*.entity.ts',
+  ],
+
+  migrations: [
+    process.env.NODE_ENV === 'production'
+      ? 'dist/src/migrations/*.js'
+      : 'src/migrations/*.ts',
+  ],
+
   synchronize: false,
+  logging: process.env.DB_LOGGING === 'true',
 });
